c28b1c9dc835691cbfee042220faa6ff95ffb688
identityapproved <identityapproved@gmail.com>
2021-08-04 12:34:13 +0300

    scrolling page

--- src/index.js
+++ src/index.js
@@ -39,33 +39,33 @@ function onInputSearch(e) {
       }
    };
    
-   function renderImg(hits) {
-      const imageCard = imageCardTemp(hits);
-      refs.gallery.insertAdjacentHTML("beforeend", imageCard);
-      const lightbox = new SimpleLightbox('.card a', { /* options */ });
-   }
+function renderImg(hits) {
+   const imageCard = imageCardTemp(hits);
+   refs.gallery.insertAdjacentHTML("beforeend", imageCard);
+   const lightbox = new SimpleLightbox('.card a', { /* options */ });
+
+   const { height: cardHeight } = document
+      .querySelector('.gallery')
+      .firstElementChild.getBoundingClientRect();
+
+   window.scrollBy({
+      top: cardHeight * 2,
+      behavior: 'smooth',
+   });
+}
    
-   function onBtnShown() {
-      if (refs.gallery.hasChildNodes()) {
-         refs.loadMoreBtn.classList.remove('visually-hidden')
-      } else {
-         refs.loadMoreBtn.classList.add('visually-hidden')
-   }
+function onBtnShown() {
+   if (refs.gallery.hasChildNodes()) {
+      refs.loadMoreBtn.classList.remove('visually-hidden')
+   } else {
+      refs.loadMoreBtn.classList.add('visually-hidden')
+}
 };
 
 function onLoadMore() {
    apiService.incrementPage();
    apiService.fetchHits().then(r => {
       renderImg(r)
-
-      const { height: cardHeight } = document
-         .querySelector('.gallery')
-         .firstElementChild.getBoundingClientRect();
-
-      window.scrollBy({
-         top: cardHeight * 2,
-         behavior: 'smooth',
-      });
    }).catch(error => {
       console.log(error)
    })
